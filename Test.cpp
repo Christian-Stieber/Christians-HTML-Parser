6a1c681e6d5b38b0a0ac7298880c5cd39709ac19
Christian Stieber <stieber.chr@gmail.com>
2023-06-04 14:53:16 +0200

    Test: revamped the Tree output, and outputting some text data now

--- Test.cpp
+++ Test.cpp
@@ -21,23 +21,92 @@ static void printAttribute(const HTMLParser::Tree::Element& element, std::string
 
 /************************************************************************/
 
-static void printElement(unsigned int indent, const HTMLParser::Tree::Element& element)
+static std::string shortText(const std::string& input)
 {
+    std::string output;
+    bool addSpace=false;
+    bool truncate=false;
+
+    for (char c : input)
+    {
+        if (c==0x0a || c==0x0d || c==0x09)
+        {
+            c=' ';
+        }
+        if (c==' ')
+        {
+            if (!output.empty())
+            {
+                addSpace=true;
+            }
+        }
+        else
+        {
+            if (truncate)
+            {
+                output.append("…");
+                break;
+            }
+            if (addSpace)
+            {
+                addSpace=false;
+                output.push_back(' ');
+            }
+            output.push_back(c);
+            if (output.size()>=80)
+            {
+                truncate=true;
+            }
+        }
+    }
+
+    return output;
+}
+
+/************************************************************************/
+
+static void printIndent(unsigned int indent)
+{
+    static const char indentString[]="   ";
+
     for (unsigned int i=0; i<indent; i++)
     {
-        std::cout << "   ";
+        std::cout << indentString;
     }
-    std::cout << element.name;
-    printAttribute(element, "id");
-    printAttribute(element, "class");
-    std::cout << std::endl;
+}
 
-    for (const auto& child : element.children)
+/************************************************************************/
+
+static void printNode(unsigned int indent, const HTMLParser::Tree::Node& node)
+{
+    auto element=dynamic_cast<const HTMLParser::Tree::Element*>(&node);
+    if (element!=nullptr)
     {
-        auto childElement=dynamic_cast<const HTMLParser::Tree::Element*>(child.get());
-        if (childElement!=nullptr)
+        printIndent(indent);
+        std::cout << "<" << element->name;
+        printAttribute(*element, "id");
+        printAttribute(*element, "class");
+        std::cout << ">" << std::endl;
+    }
+    else
+    {
+        auto text=dynamic_cast<const HTMLParser::Tree::Text*>(&node);
+        if (text!=nullptr)
         {
-            printElement(indent+1, *childElement);
+            auto output=shortText(text->text);
+            if (!output.empty())
+            {
+                printIndent(indent);
+                std::cout << "\"" << output << "\"" << std::endl;
+            }
+        }
+    }
+
+    if (element!=nullptr)
+    {
+        for (const auto& child : element->children)
+        {
+            printNode(indent+1, *child);
         }
     }
 }
@@ -52,6 +121,6 @@ int main()
     std::stringstream file;
     file << std::cin.rdbuf();
     HTMLParser::Parser parser(file.view());
-    printElement(0,*parser.document.html);
+    printNode(0,*parser.document.html);
     return 0;
 }
