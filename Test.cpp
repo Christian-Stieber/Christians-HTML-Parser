1a9c49f5f1b81ba88f7315f3ee1baa5da7796386
Christian Stieber <stieber.chr@gmail.com>
2023-06-04 02:27:37 +0200

    Test: added some partial tree printing

--- Test.cpp
+++ Test.cpp
@@ -1,12 +1,46 @@
+#include "Headers/Parser.hpp"
+
 #include <iostream>
 #include <sstream>
-
-#include "Headers/Parser.hpp"
 
 /************************************************************************/
 /*
  * g++ -ggdb -std=c++20 Test.cpp
  */
+
+/************************************************************************/
+
+static void printAttribute(const HTMLParser::Tree::Element& element, std::string name)
+{
+    auto iterator=element.attributes.find(name);
+    if (iterator!=element.attributes.end())
+    {
+        std::cout << " [" << iterator->first << "=" << iterator->second << "]";
+    }
+}
+
+/************************************************************************/
+
+static void printElement(unsigned int indent, const HTMLParser::Tree::Element& element)
+{
+    for (unsigned int i=0; i<indent; i++)
+    {
+        std::cout << "   ";
+    }
+    std::cout << element.name;
+    printAttribute(element, "id");
+    printAttribute(element, "class");
+    std::cout << std::endl;
+
+    for (const auto& child : element.children)
+    {
+        auto childElement=dynamic_cast<const HTMLParser::Tree::Element*>(child.get());
+        if (childElement!=nullptr)
+        {
+            printElement(indent+1, *childElement);
+        }
+    }
+}
 
 /************************************************************************/
 /*
@@ -18,5 +52,6 @@ int main()
     std::stringstream file;
     file << std::cin.rdbuf();
     HTMLParser::Parser parser(file.view());
+    printElement(0,*parser.document.html);
     return 0;
 }
